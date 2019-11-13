be3b06fabac3d8d4bb6b2ec0f14b47774603f19b
manuel.werder <manuel.werder@csbe.ch>
2019-11-13 12:50:45 +0100

    Apps are installed in Disaster settings.py

--- Disaster/settings.py
+++ Disaster/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Newsletter_Overview',
+    'Newsletter_Sender',
+    'MJML_Editor',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
