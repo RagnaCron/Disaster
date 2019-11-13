db2e8a5c2183e8878bedef3068fdbbb26b3c3486
manuel.werder <manuel.werder@csbe.ch>
2019-11-13 11:08:55 +0100

    Init three Django Apps

---
+++ MJML_Editior/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MjmlEditiorConfig(AppConfig):
+    name = 'MJML_Editior'
---
+++ Newsletter_Sender/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NewsletterSenderConfig(AppConfig):
+    name = 'Newsletter_Sender'
