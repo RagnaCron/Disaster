561aa0f4db513a78c17eb01d4b21c7ae566b5b99
nsinghsingh <qweras%7>
2019-11-18 06:40:50 +0100

    added placeholder for the templates

--- MJML_Editor/static/MJML_Editor/Create.js
+++ MJML_Editor/static/MJML_Editor/Create.js
@@ -1,9 +1,87 @@
 let workPlace = document.getElementById("workPlace");
 
 let spalte1Template;
+let spalte2Template;
+let spalte3Template;
+let trennlinieTemplate;
 
 function initializeTemplate1() {
     spalte1Template = `
+<!-- Side image -->
+<mj-section background-color="white">
+    <!-- Left image -->
+    <mj-column>
+        <mj-image width="200px" src="https://designspell.files.wordpress.com/2012/01/sciolino-paris-bw.jpg" />
+    </mj-column>
+    <!-- right paragraph -->
+    <mj-column>
+        <mj-text font-style="italic"
+            font-size="20px"
+            font-family="Helvetica Neue"
+            color="#626262">
+            <mj-raw>
+            <div id="editor"></div>
+                </mj-raw>
+        </mj-text>
+        <mj-text color="#525252">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum enim eget magna efficitur, eu semper augue semper. Aliquam erat volutpat. Cras id dui lectus. Vestibulum sed finibus lectus.</mj-text>
+    </mj-column>
+</mj-section>
+`;
+}
+
+function initializeTemplate2() {
+    spalte2Template = `
+<!-- Side image -->
+<mj-section background-color="white">
+    <!-- Left image -->
+    <mj-column>
+        <mj-image width="200px" src="https://designspell.files.wordpress.com/2012/01/sciolino-paris-bw.jpg" />
+    </mj-column>
+    <!-- right paragraph -->
+    <mj-column>
+        <mj-text font-style="italic"
+            font-size="20px"
+            font-family="Helvetica Neue"
+            color="#626262">
+            <mj-raw>
+            <div id="editor"></div>
+                </mj-raw>
+        </mj-text>
+        <mj-text color="#525252">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum enim eget magna efficitur, eu semper augue semper. Aliquam erat volutpat. Cras id dui lectus. Vestibulum sed finibus lectus.</mj-text>
+    </mj-column>
+</mj-section>
+`;
+}
+
+function initializeTemplate3() {
+    spalte3Template = `
+<!-- Side image -->
+<mj-section background-color="white">
+    <!-- Left image -->
+    <mj-column>
+        <mj-image width="200px" src="https://designspell.files.wordpress.com/2012/01/sciolino-paris-bw.jpg" />
+    </mj-column>
+    <!-- right paragraph -->
+    <mj-column>
+        <mj-text font-style="italic"
+            font-size="20px"
+            font-family="Helvetica Neue"
+            color="#626262">
+            <mj-raw>
+            <div id="editor"></div>
+                </mj-raw>
+        </mj-text>
+        <mj-text color="#525252">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum enim eget magna efficitur, eu semper augue semper. Aliquam erat volutpat. Cras id dui lectus. Vestibulum sed finibus lectus.</mj-text>
+    </mj-column>
+</mj-section>
+`;
+}
+
+function initializeTrennlinie() {
+    trennlinieTemplate = `
 <!-- Side image -->
 <mj-section background-color="white">
     <!-- Left image -->
@@ -42,6 +120,18 @@ function drop(event) {
     if (id === "spalte1") {
         initializeTemplate1();
         dragged = spalte1Template;
+    }
+    else if (id === "spalte2") {
+        initializeTemplate2();
+        dragged = spalte2Template;
+    }
+    else if (id === "spalte3") {
+        initializeTemplate3();
+        dragged = spalte3Template;
+    }
+    else if (id === "trennlinie") {
+        initializeTrennlinie();
+        dragged = trennlinieTemplate;
     }
     if (workPlace.childElementCount > 0) {
         for (let i = 0; i < workPlace.childElementCount; i++) {
